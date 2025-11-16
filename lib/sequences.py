8466dc1334577728c0f4b044769d6eeac8ca4340
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-11-16 19:50:11 +0300

    update fixed sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        fib = []
+    else:
+        fib = [0]
+        if length > 1:
+            fib.append(1)
+            for i in range(2, length):
+                fib.append(fib[i-1] + fib[i-2])
+    print(fib)
